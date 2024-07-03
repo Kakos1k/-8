fdf002793f9fa386965f1d9f0e58845f5d7a6196
Kakos1k <168650662+Kakos1k@users.noreply.github.com>
2024-07-03 14:43:18 +0300

    script.js

--- script.js
+++ script.js
@@ -1,31 +1,28 @@
-// Первая карточка - Игра УГАДАЙКА, кнопка НАЧАТЬ ИГРУ
-// Клик по кнопке НАЧАТЬ ИГРУ
-// Диапазон значений Введите минимальное и максимальное значение числа для игры (от -999 до 999), кнопка ПРОДОЛЖИТЬ
-document.getElementById('btnTobegin').addEventListener('click', function () {   // Клик по кнопке НАЧАТЬ ИГРУ --> Переход на карточку Диапазон значений
-    document.querySelector('.title-page').classList.add('hidden');              // Игра УГАДАЙКА / добавляется класс hidden; надпись исчезает
-    document.querySelector('.value-range').classList.remove('hidden');          // Диапазон значений / класс hidden удаляется; надпись появляется
-    document.querySelector('.valueRange').classList.remove('hidden');           // Диапазон значений Текст / класс hidden удаляется; надпись появляется
-    document.querySelector('.form-inline').classList.remove('hidden');          // Форма мин мах / класс hidden удаляется; форма появляется
-    document.querySelector('#btnTobegin').classList.add('hidden');              // Кнопка НАЧАТЬ ИГРУ / добавляется класс hidden; кнопка исчезает
-    document.querySelector('#btnProceed').classList.remove('hidden');           // Кнопка ПРОДОЛЖИТЬ / класс hidden удаляется; кнопка появляется
+
+document.getElementById('btnTobegin').addEventListener('click', function () {  
+    document.querySelector('.title-page').classList.add('hidden');              
+    document.querySelector('.value-range').classList.remove('hidden');          
+    document.querySelector('.valueRange').classList.remove('hidden');           
+    document.querySelector('.form-inline').classList.remove('hidden');          
+    document.querySelector('#btnTobegin').classList.add('hidden');              
+    document.querySelector('#btnProceed').classList.remove('hidden');           
 })
 
-// Клик по кнопке ПРОДОЛЖИТЬ
-// Условия, Загадайте любое целое число от 0 до 100, а я его угадаю, кнопка ИГРАТЬ
-document.getElementById('btnProceed').addEventListener('click', function () {   // Клик по кнопке ПРОДОЛЖИТЬ --> Переход на карточку Условия с кнопкой ИГРАТЬ
-    document.querySelector('.value-range').classList.add('hidden');             // Диапазон значений / опять добавляется класс hidden; надпись исчезает
-    document.querySelector('.terms').classList.remove('hidden');                // Условия / класс hidden удаляется; надпись появляется
-    document.querySelector('.valueRange').classList.add('hidden');              // Диапазон значений Текст / опять добавляется класс hidden; надпись исчезает
-    document.querySelector('.form-inline').classList.add('hidden');             // Форма мин мах / опять добавляется класс hidden; форма исчезает
-    document.querySelector('.guessNumber').classList.remove('hidden');          // Фраза Загадайте любое целое число / класс hidden удаляется; Фраза появляется
-    document.querySelector('#btnProceed').classList.add('hidden');              // Кнопка ПРОДОЛЖИТЬ / опять добавляется класс hidden; кнопка исчезает
-    document.querySelector('#btnPlay').classList.remove('hidden');              // Кнопка ИГРАТЬ / класс hidden удаляется; кнопка появляется
+
+document.getElementById('btnProceed').addEventListener('click', function () {   
+    document.querySelector('.value-range').classList.add('hidden');             
+    document.querySelector('.terms').classList.remove('hidden');               
+    document.querySelector('.valueRange').classList.add('hidden');              
+    document.querySelector('.form-inline').classList.add('hidden');             
+    document.querySelector('.guessNumber').classList.remove('hidden');          
+    document.querySelector('#btnProceed').classList.add('hidden');              
+    document.querySelector('#btnPlay').classList.remove('hidden');              
     minValue = parseInt(document.querySelector('#formInputMin').value);
     maxValue = parseInt(document.querySelector('#formInputMax').value);
     minValue = (minValue < -999) ? minValue = -999 : (minValue > 999) ? minValue = 999 : minValue;
     maxValue = (maxValue > 999) ? maxValue = 999 : (maxValue < -999) ? maxValue = -999 : maxValue;
     if (maxValue < minValue) {
-        [maxValue, minValue] = [minValue, maxValue]; // Значения меняются местами если max меньше min.
+        [maxValue, minValue] = [minValue, maxValue];
     }
     if (Number.isNaN(maxValue) || Number.isNaN(minValue)) {
         minValue = 0;
@@ -34,34 +31,33 @@ document.getElementById('btnProceed').addEventListener('click', function () {   
     guessNumber.innerText = `Загадайте любое целое число от ${minValue} до ${maxValue}, а я его угадаю`;
 })
 
-// Клик по кнопке ИГРАТЬ
-// Сама Игра - Карточка с Вопросом, кнопками: меньше, больше, верно, заново
-document.getElementById('btnPlay').addEventListener('click', function () {      // Клик по кнопке ИГРАТЬ --> Переход на карточку Вопрос №, с кнопками: меньше, больше, верно, заново
-    document.querySelector('.terms').classList.add('hidden');                   // Условия / опять добавляется класс hidden; надпись исчезает
-    document.querySelector('.question').classList.remove('hidden');             // Вопрос № / класс hidden удаляется; надпись появляется
-    document.querySelector('.guessNumber').classList.add('hidden');             // Фраза Загадайте любое целое число / опять добавляется класс hidden; надпись исчезает
-    document.querySelector('.no-gutters').classList.remove('hidden');           // Вы загадали число / класс hidden удаляется; надпись появляется
-    document.querySelector('#btnPlay').classList.add('hidden');                 // Кнопка ИГРАТЬ / опять добавляется класс hidden; кнопка исчезает
-    document.querySelector('#btnLess').classList.remove('hidden');              // Кнопка меньше / класс hidden удаляется; кнопка появляется
-    document.querySelector('#btnEqual').classList.remove('hidden');             // Кнопка Верно! / класс hidden удаляется; кнопка появляется
-    document.querySelector('#btnOver').classList.remove('hidden');              // Кнопка больше / класс hidden удаляется; кнопка появляется
-    document.querySelector('.btn-link').classList.remove('hidden');             // Кнопка Заново / класс hidden удаляется; кнопка появляется
 
-    let answerNumber = Math.floor((minValue + maxValue) / 2); // Середина числового диапазона
-    let orderNumber = 1; // Номер первого вопроса.
+document.getElementById('btnPlay').addEventListener('click', function () {      
+    document.querySelector('.terms').classList.add('hidden');                  
+    document.querySelector('.question').classList.remove('hidden');            
+    document.querySelector('.guessNumber').classList.add('hidden');            
+    document.querySelector('.no-gutters').classList.remove('hidden');           
+    document.querySelector('#btnPlay').classList.add('hidden');                
+    document.querySelector('#btnLess').classList.remove('hidden');             
+    document.querySelector('#btnEqual').classList.remove('hidden');            
+    document.querySelector('#btnOver').classList.remove('hidden');             
+    document.querySelector('.btn-link').classList.remove('hidden');            
+
+    let answerNumber = Math.floor((minValue + maxValue) / 2); 
+    let orderNumber = 1; 
     let gameRun = true;
 
-    const orderNumberField = document.getElementById('orderNumberField'); // Вопрос №_
+    const orderNumberField = document.getElementById('orderNumberField'); 
     const answerField = document.getElementById('answerField');
 
-    // Преобразования числа в текстовую форму. Число выводится в текстовой форме, если в текстовой форме меньше 20 символов, включая пробелы.
+    /
 
     let units = ['', 'один', 'два', 'три', 'четыре', 'пять', 'шесть', 'семь', 'восемь', 'девять'];
     let teens = ['', 'десять', 'одинадцать', 'двенадцать', 'тринадцать', 'четырнадцать', 'пятнадцать', 'шестнадцать', 'семнадцать', 'восемнадцать', 'девятнадцать'];
     let dozens = ['', 'двадцать', 'тридцать', 'сорок', 'пятьдесят', 'шестьдесят', 'семьдесят', 'восемьдесят', 'девяносто'];
     let hundreds = ['', 'сто', 'двести', 'триста', 'четыреста', 'пятьсот', 'шестьсот', 'семьсот', 'восемьсот', 'девятьсот'];
 
-    function numberToText() { // Функция преобразования числа из цифр в слова (числа от -999 до 999).
+    function numberToText() { 
         let number = Math.abs(answerNumber);
         let text = '';
 
@@ -87,7 +83,7 @@ document.getElementById('btnPlay').addEventListener('click', function () {      
         }
     }
 
-    function numberToTextHundreds() { // Функция вычисления остатка от сотого числа и преобразования его в числа из цифр в слова (числа от 0 до 99) для последующего присоединения к функции numberToText() расчитывающей сотни hundreds.
+    function numberToTextHundreds() { 
         let unitsTeensDozens = Math.abs(answerNumber) % 100;
 
         if (unitsTeensDozens <= 9) {
@@ -103,11 +99,10 @@ document.getElementById('btnPlay').addEventListener('click', function () {      
         }
     }
 
-    orderNumberField.innerText = orderNumber; // Вопрос № 1
+    orderNumberField.innerText = orderNumber;
     answerField.innerText = answerNumber >= 0 ? numberToText().length < 20 && answerNumber >= 0 ? `Вы загадали число ${numberToText()}?` : `Вы загадали число ${answerNumber}?` : numberToText().length < 20 ? `Вы загадали число минус ${numberToText()}?` : `Вы загадали число ${answerNumber}?`;
-    // Вы загадали число [__]? - Середина числового диапазона (answerNumber - подставляется середина числового диапазона в функцию numberToText() которая преобразует в текст и записывается в переменную numberWord).
-
-    document.getElementById('btnLess').addEventListener('click', function () { // Код для кнопки «Меньше».
+   
+    document.getElementById('btnLess').addEventListener('click', function () { 
         if (gameRun) {
             if (minValue === maxValue || minValue == answerNumber) {
                 const phraseRandom = Math.round(Math.random() * 3);
@@ -131,11 +126,11 @@ document.getElementById('btnPlay').addEventListener('click', function () {      
                 answerField.innerText = answerPhrase;
                 gameRun = false;
             } else {
-                maxValue = answerNumber - 1; // Изменение верхней границы поискового диапазона.
+                maxValue = answerNumber - 1; 
                 answerNumber = Math.floor((minValue + maxValue) / 2);
                 orderNumber++;
                 orderNumberField.innerText = orderNumber;
-                const phraseRandom = Math.round(Math.random() * 4); // Генерируется случайное число от 0 до 4.
+                const phraseRandom = Math.round(Math.random() * 4); 
                 switch (phraseRandom) {
                     case 1:
                         answerPhrase = `Наверное, это число `
@@ -158,8 +153,7 @@ document.getElementById('btnPlay').addEventListener('click', function () {      
         }
     })
 
-    document.getElementById('btnOver').addEventListener('click', function () { // Код для кнопки «Больше».
-        if (gameRun) {
+    document.getElementById('btnOver').addEventListener('click', function () { 
             if (minValue === maxValue) {
                 const phraseRandom = Math.round(Math.random() * 3);
                 switch (phraseRandom) {
@@ -182,11 +176,11 @@ document.getElementById('btnPlay').addEventListener('click', function () {      
                 answerField.innerText = answerPhrase;
                 gameRun = false;
             } else {
-                minValue = answerNumber + 1; // Изменение нижней границы поискового диапазона.
+                minValue = answerNumber + 1;
                 answerNumber = Math.floor((minValue + maxValue) / 2);
                 orderNumber++;
                 orderNumberField.innerText = orderNumber;
-                const phraseRandom = Math.round(Math.random() * 4); // Генерируется случайное число от 0 до 4.
+                const phraseRandom = Math.round(Math.random() * 4); 
                 switch (phraseRandom) {
                     case 0:
                         answerPhrase = `Вы загадали число `
@@ -213,8 +207,7 @@ document.getElementById('btnPlay').addEventListener('click', function () {      
         }
     })
 
-    document.getElementById('btnEqual').addEventListener('click', function () { // Код для кнопки "Верно".
-        if (gameRun) {
+    document.getElementById('btnEqual').addEventListener('click', function () { 
             const phraseRandom = Math.round(Math.random() * 3);
             switch (phraseRandom) {
                 case 0:
@@ -239,25 +232,24 @@ document.getElementById('btnPlay').addEventListener('click', function () {      
     })
 })
 
-// Клик по кнопке Заново
-// Диапазон значений Введите минимальное и максимальное значение числа для игры (от -999 до 999), кнопка ПРОДОЛЖИТЬ
-document.getElementById('btnRetry').addEventListener('click', function () {     // Клик по кнопке Заново --> Переход на карточку Диапазон значений
-    document.querySelector('.question').classList.toggle('hidden');             // Вопрос № / опять добавляется класс hidden; надпись исчезает
-    document.querySelector('.value-range').classList.toggle('hidden');          // Диапазон значений / класс hidden удаляется; надпись появляется
-    document.querySelector('.no-gutters').classList.toggle('hidden');           // Вы загадали число / опять добавляется класс hidden; надпись исчезает
-    document.querySelector('.valueRange').classList.toggle('hidden');           // Диапазон значений Текст / класс hidden удаляется; надпись появляется
-    document.querySelector('.form-inline').classList.toggle('hidden');          // Форма мин мах / класс hidden удаляется; форма появляется
-    document.querySelector('#btnLess').classList.toggle('hidden');              // Кнопка меньше / опять добавляется класс hidden; кнопка исчезает
-    document.querySelector('#btnEqual').classList.toggle('hidden');             // Кнопка Верно! / опять добавляется класс hidden; кнопка исчезает
-    document.querySelector('#btnOver').classList.toggle('hidden');              // Кнопка больше / опять добавляется класс hidden; кнопка исчезает
-    document.querySelector('.btn-link').classList.toggle('hidden');             // Кнопка Заново / опять добавляется класс hidden; кнопка исчезает
-    document.querySelector('#btnProceed').classList.toggle('hidden');           // Кнопка Продолжить / класс hidden удаляется; кнопка появляется
+
+document.getElementById('btnRetry').addEventListener('click', function () {    
+    document.querySelector('.question').classList.toggle('hidden');            
+    document.querySelector('.value-range').classList.toggle('hidden');        
+    document.querySelector('.no-gutters').classList.toggle('hidden');           
+    document.querySelector('.valueRange').classList.toggle('hidden');          
+    document.querySelector('.form-inline').classList.toggle('hidden');         
+    document.querySelector('#btnLess').classList.toggle('hidden');             
+    document.querySelector('#btnEqual').classList.toggle('hidden');            
+    document.querySelector('#btnOver').classList.toggle('hidden');              
+    document.querySelector('.btn-link').classList.toggle('hidden');             
+    document.querySelector('#btnProceed').classList.toggle('hidden');          
     document.querySelector('#formInputMin').value = '';
     document.querySelector('#formInputMax').value = '';
     minValue = (minValue < -999) ? minValue = -999 : (minValue > 999) ? minValue = 999 : minValue;
     maxValue = (maxValue > 999) ? maxValue = 999 : (maxValue < -999) ? maxValue = -999 : maxValue;
     if (maxValue < minValue) {
-        [maxValue, minValue] = [minValue, maxValue]; // Значения меняются местами если max меньше min.
+        [maxValue, minValue] = [minValue, maxValue]; 
     }
     if (Number.isNaN(maxValue) || Number.isNaN(minValue)) {
         minValue = 0;
@@ -265,15 +257,14 @@ document.getElementById('btnRetry').addEventListener('click', function () {     
     }
     guessNumber.innerText = `Загадайте любое целое число от ${minValue} до ${maxValue}, а я его угадаю`;
 
-    // Клик по кнопке ПРОДОЛЖИТЬ
-    // Условия, Загадайте любое целое число от 0 до 100, а я его угадаю, кнопка ИГРАТЬ
-    document.getElementById('btnProceed').addEventListener('click', function () {   // Клик по кнопке ПРОДОЛЖИТЬ --> Переход на карточку Условия с кнопкой ИГРАТЬ
-        document.querySelector('.value-range').classList.add('hidden');             // Диапазон значений / опять добавляется класс hidden; надпись исчезает
-        document.querySelector('.terms').classList.remove('hidden');                // Условия / класс hidden удаляется; надпись появляется
-        document.querySelector('.valueRange').classList.add('hidden');              // Диапазон значений Текст / опять добавляется класс hidden; надпись исчезает
-        document.querySelector('.form-inline').classList.add('hidden');             // Форма min max / опять добавляется класс hidden; форма исчезает
-        document.querySelector('.guessNumber').classList.remove('hidden');          // Фраза Загадайте любое целое число / класс hidden удаляется; Фраза появляется
-        document.querySelector('#btnProceed').classList.add('hidden');              // Кнопка ПРОДОЛЖИТЬ / опять добавляется класс hidden; кнопка исчезает
-        document.querySelector('#btnPlay').classList.remove('hidden');              // Кнопка ИГРАТЬ / класс hidden удаляется; кнопка появляется
+   
+    document.getElementById('btnProceed').addEventListener('click', function () {  
+        document.querySelector('.value-range').classList.add('hidden');             
+        document.querySelector('.terms').classList.remove('hidden');                
+        document.querySelector('.valueRange').classList.add('hidden');              
+        document.querySelector('.form-inline').classList.add('hidden');             
+        document.querySelector('.guessNumber').classList.remove('hidden');          
+        document.querySelector('#btnProceed').classList.add('hidden');              
+        document.querySelector('#btnPlay').classList.remove('hidden');              
     })
 })
